91bf40d45e07407729e3cd55ad922662a2dfc2fc
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-02-12 11:45:55 +0100

    first commit

---
+++ MainWindow.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using Microsoft.Win32;
+
+namespace desktop_app
+{
+    public partial class MainWindow : Form
+    {
+        public ChromiumWebBrowser browser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GoFullscreen(true);
+            InitBrowser();
+            AddApplicationToStartup();
+        }
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.google.com");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GoFullscreen(bool fullscreen)
+        {
+            if (fullscreen)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                this.Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            }
+        }
+
+        public static void AddApplicationToStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue("My Program", "\"" + Application.ExecutablePath + "\"");
+            }
+        }
+
+    }
+}
